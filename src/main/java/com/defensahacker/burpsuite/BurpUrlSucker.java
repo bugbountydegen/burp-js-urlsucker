6a8e954fa7ebcab51e694460bca71a341e78fbed
bugbountydegen <bugbounty@defensahacker.com>
2025-09-22 18:32:48 +0200

    First version

--- src/main/java/com/defensahacker/burpsuite/BurpUrlSucker.java
+++ src/main/java/com/defensahacker/burpsuite/BurpUrlSucker.java
@@ -1,3 +1,13 @@
+/**
+ * BurpUrlSucker.java
+ *
+ * Burpsuite (Montoya version) that finds passively hidden endpoints and urls within any downloaded JavaScript file while browsing a website.
+ *
+ * Based on previous Perl Script: https://github.com/defensahacker/URLSUCKER
+ *
+ * (c) 2025 Defensahacker Labs
+ */
+
 package com.defensahacker.burpsuite;
 
 import burp.api.montoya.BurpExtension;
